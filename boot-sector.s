fd9322acf8a0fe1bb82d3d89c8bbf92af830d38d
yigitkader <ryigitkader@gmail.com>
2020-12-06 21:22:27 +0300

    boot-sector initialized

--- boot-sector.s
+++ boot-sector.s
@@ -1,11 +1,31 @@
-;2 byte endless loop for security our ram
-endlessLoop:
-    jmp endlessLoop  
 
-times 510 - ($ -$$) db 0x00 ;508 byte
-dw 0xaa55 ;2byte
+bits    16                ; 16 Bit Real Mod
+org     0x7c00            ; 0x7C00 loaded address
 
-;db 0x55
-;db 0xaa
+start:
+    jmp loader
+
+msg db "kaOS Loading....", 0; message
+
+write:
+    lodsb                ; next byte of word  si -> al
+    or al,al              ; al is Zero ?
+    jz endwrite           ; if js is true go to endwrite
+    mov ah,0x0e           ; function
+    int 0x10              ; interrupt
+    jmp write             ; call itself again
+
+endwrite:
+    ret                   ; done
+
+
+loader:
+    mov si,msg            ; Transfer the message to be written to the index recorder (si)
+    call write            ; call write label
+    cli                   ; clear interrupts
+    hlt                   ; halt system
+
+times 510 -($-$$) db 0x00
+dw 0xaa55                 ; signature of boot
 
 
